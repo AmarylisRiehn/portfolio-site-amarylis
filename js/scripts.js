61ce142cf7d83042ff92916cf4806d15ad057769
AmarylisRiehn <amaryliscreates@gmail.com>
2023-06-26 23:16:59 -0500

    js fix attempt

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 function menuToggle() {
   var x = document.getElementById("mynavtoggle");
-  if (x.className === "navtoggle") {
+  var isMobileBreakpoint = window.innerWidth <= 641
+ 
+  if (x.className === "navtoggle" && isMobileBreakpoint) {
     x.className += " responsive";
   } else {
     x.className = "navtoggle";
